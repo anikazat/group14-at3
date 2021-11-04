04eb4230d0ac41f6cf7a647e1b27f3c0f85853f8
Anika Zatschler <Anika.Zatschler@student.uts.edu.au>
2021-11-04 22:08:52 +1100

    update data.py

--- src/data.py
+++ src/data.py
@@ -2,34 +2,18 @@ import streamlit as st
 from dataclasses import dataclass
 import pandas as pd
 
-csv_file = st.file_uploader("Choose a CSV file")
-if csv_file is not None:
-    df = pd.read_csv(csv_file)
-else:
-    st.write('No csv file uploaded')
-    st.title('Overall Information')
-    st.write(f'**Name of Table:** {Dataset.get_name(Dataset)}')
-    st.write(f'**Number of Rows:** {Dataset.get_n_rows(df)}')
-    st.write(f'**Number of Columns:** {Dataset.get_n_cols(df)}')
-    st.write(f'**Number of Duplicated Rows:** {print(Dataset.get_n_duplicates(df))}')
-    st.write(f'**Number of Rows with Missing Values:** {Dataset.get_n_missing(df)}')
-    st.write(f'**List of Columns:** {Dataset.get_cols_list(df)}')
-    st.write('**Types of Columns:**', Dataset.get_cols_dtype(df))
-    show_nrows = st.slider('Select the number of rows to be displayed', 5, 50, 5)
-    select_column = st.multiselect('Which columns do you want to convert to dates', df.select_dtypes(include='object').columns, on_change=form_callback)
-
-if any(ele in select_column for ele in df.columns):
-    for i in select_column:
-        x = datetime_fun(i)
-        df.append(x)
-    st.write('Top Rows of Table', Dataset.get_head(df, show_nrows))
-    st.write('Bottom Rows of Table', Dataset.get_tail(df, show_nrows))
-    st.write('Random Sample Rows of Table', Dataset.get_sample(df, show_nrows))
-else:
-    st.write('Top Rows of Table', Dataset.get_head(df, show_nrows))
-    st.write('Bottom Rows of Table', Dataset.get_tail(df, show_nrows))
-    st.write('Random Sample Rows of Table', Dataset.get_sample(df, show_nrows))
-
+def read_data(n_data, c_data):
+    ls = Dataset(n_data, c_data)
+    st.subheader('1. Overall Information')
+    st.write(f'**Name of Table:** {ls.get_name()}')
+    st.write(f'**Number of Rows:** {ls.get_n_rows()}')
+    st.write(f'**Number of Columns:** {ls.get_n_cols()}')
+    st.write(f'**Number of Duplicated Rows:** {ls.get_n_duplicates()}')
+    st.write(f'**Number of Rows with Missing Values:** {ls.get_n_missing()}')
+    st.write('**List of Columns:** ')
+    st.text(', '.join(ls.get_cols_list()))
+    st.write('**Types of Columns:**')
+    st.dataframe(pd.DataFrame.from_dict(ls.get_cols_dtype(), orient='index', columns=(['type'])))
 
 @dataclass
 class Dataset:
@@ -40,7 +24,6 @@ class Dataset:
     """
     Return filename of loaded dataset
     """
-    self.name = csv_file.name
     return self.name
 
   def get_n_rows(self):
@@ -60,19 +43,19 @@ class Dataset:
     """
       Return list column names of loaded dataset
     """
-    return ', '.join(list(self.df.keys()))
+    return list(self.df.keys())
 
   def get_cols_dtype(self):
     """
       Return dictionary with column name as keys and data type as values
     """
-    return dict(self.df.dtypes)
+    return self.df.dtypes.apply(lambda x: x.name).to_dict()
 
   def get_n_duplicates(self):
     """
       Return number of duplicated rows of loaded dataset
     """
-    return df.duplicated
+    return self.df.duplicated().sum()
 
   def get_n_missing(self):
     """
@@ -102,52 +85,17 @@ class Dataset:
     """
       Return list column names of numeric type from loaded dataset
     """
-    return ', '.join(list(self.df.select_dtypes(include=['int64','float64']).columns))
+    return list(self.df.select_dtypes(include=['int64','float64']).columns)
 
   def get_text_columns(self):
     """
       Return list column names of text type from loaded dataset
     """
-    return ', '.join(list(self.df.select_dtypes(include=['O']).columns))
+    return list(self.df.select_dtypes(include=['O']).columns)
 
   def get_date_columns(self):
     """
       Return list column names of datetime type from loaded dataset
     """
-    return ', '.join(list(self.df.select_dtypes(include=['datetime64[ns]']).columns))
-
-# Everything below is to check that the functions work. Will move this before submission
-def datetime_fun(column):
-    """ 
-    function that will convert a colomn to datetime format
-    """
-    df[column] = pd.to_datetime(df[column], format='%Y-%m-%d %H:%M:%S')
-    return(df)
-
-def form_callback():
-    #st.write(st.session_state.select_column)
-    st.session_state.select_column = select_column
-
-st.title('Overall Information')
-st.write(f'**Name of Table:** {Dataset.get_name(Dataset)}')
-st.write(f'**Number of Rows:** {Dataset.get_n_rows(df)}')
-st.write(f'**Number of Columns:** {Dataset.get_n_cols(df)}')
-st.write(f'**Number of Duplicated Rows:** {print(Dataset.get_n_duplicates(df))}')
-st.write(f'**Number of Rows with Missing Values:** {Dataset.get_n_missing(df)}')
-st.write(f'**List of Columns:** {Dataset.get_cols_list(df)}')
-st.write('**Types of Columns:**', Dataset.get_cols_dtype(df))
-show_nrows = st.slider('Select the number of rows to be displayed', 5, 50, 5)
-select_column = st.multiselect('Which columns do you want to convert to dates', df.select_dtypes(include='object').columns, on_change=form_callback)
-
-if any(ele in select_column for ele in df.columns):
-    for i in select_column:
-        x = datetime_fun(i)
-        df.append(x)
-    st.write('Top Rows of Table', Dataset.get_head(df, show_nrows))
-    st.write('Bottom Rows of Table', Dataset.get_tail(df, show_nrows))
-    st.write('Random Sample Rows of Table', Dataset.get_sample(df, show_nrows))
-else:
-    st.write('Top Rows of Table', Dataset.get_head(df, show_nrows))
-    st.write('Bottom Rows of Table', Dataset.get_tail(df, show_nrows))
-    st.write('Random Sample Rows of Table', Dataset.get_sample(df, show_nrows))
+    return list(self.df.select_dtypes(include=['datetime64[ns]']).columns)
 
