5106a63283acb47184e6909f1b01c6819ba9de31
EzgiKanli <79822621+EzgiKanli@users.noreply.github.com>
2021-11-05 18:44:37 +1100

    Update text.py

--- src/text.py
+++ src/text.py
@@ -3,29 +3,6 @@ import streamlit as st
 from dataclasses import dataclass
 from pandas import Series,DataFrame
 
-def read_text(n_data):
-    column = n_data.columns
-    n=0
-    for i in column:
-        if n_data[(f'{i}')].dtype == int or n_data[(f'{i}')].dtype == object:
-          ls = TextColumn((f'{i}'),pd.Series(n_data[(f'{i}')].values))
-          st.subheader(f'3.{n} Field Name: {ls.col_name}')
-          st.dataframe(ls.table())
-          st.markdown(f'* Bar Chart')
-
-          if ls.get_missing() == len(ls.serie):
-             st.write(f'Can not create Bar_chart without values')
-          else:
-             st.pyplot(ls.get_barchart())
-
-          st.markdown(f'* Most Frequent Values')
-          if ls.get_missing() == len(ls.serie):
-            st.markdown("No frequent values")
-          else:
-            st.dataframe(ls.frequent().head(20))
-          n=n+1
-
-        else:pass
 
 @dataclass
 class TextColumn:
@@ -96,51 +73,40 @@ class TextColumn:
       Frequency_percentage = self.data.value_counts()/len(self.data)
       occurence = self.data.value_counts()
       u=pd.concat([occurence,Frequency_percentage],axis = 1,keys =['Occurence','Frequency_percentage'])
-      return u
- 
+      return u 
 
 
-def main():
-  html_temp ="""
-  <div style ="">
-  <h1 style ="color:black;text-align:center;"> </h1>
-  </div>
-  """
+def text_main(z):
 
-  st.markdown(html_temp,unsafe_allow_html = True)
-  uploaded_file = st.file_uploader("Choose a file")
-  if uploaded_file is not None:
-    df = pd.read_csv(uploaded_file)
-    st.write(df)
-    Field = st.selectbox('Field Name',(df.columns))
-    name = Field
-    s = pd.Series(df[name].values)
-    
-    t =TextColumn(s)
+  df = pd.read_csv(z)
+  st.write(df)
+  for i in df.columns:
+    s = pd.Series(df[i].values)
+    if s.dtypes =='object':
+      t =TextColumn(s)
 
-    test_data={'Value':{'number of unique values':t.get_unique(),
-    'number of missing values': t.get_missing(),
-    'number of empty rows': t.get_empty(),
-    'number of whitespace rows':t.get_whitespace(),
-    'number of lower case rows': t.get_lowercase(),
-    'number of upper case rows': t.get_uppercase(),
-    'number of alphabet rows': t.get_alphabet(),
-    'number of digit rows': t.get_digit(),
-    'Mode for selected column': t.get_mode()}}
+      test_data={'Value':{'number of unique values':t.get_unique(),
+      'number of missing values': t.get_missing(),
+      'number of empty rows': t.get_empty(),
+      'number of whitespace rows':t.get_whitespace(),
+      'number of lower case rows': t.get_lowercase(),
+      'number of upper case rows': t.get_uppercase(),
+      'number of alphabet rows': t.get_alphabet(),
+      'number of digit rows': t.get_digit(),
+      'Mode for selected column': t.get_mode()}}
 
-    full=DataFrame(test_data)
-    table1=full.astype(str)
-    st.subheader("Exploratory Data Analysis")
-    st.dataframe(table1)
+      full=DataFrame(test_data)
+      table1=full.astype(str)
+      st.subheader("Exploratory Data Analysis")
+      st.dataframe(table1)
 
-    st.subheader("Bar Chart")
-    st.bar_chart(t.get_barchart())
+      st.subheader("Bar Chart")
+      st.bar_chart(t.get_barchart())
 
-    st.subheader("Most Frequent Values")
-    table2 = t.get_frequent()
-    freq_table = table2.astype(str)
-    st.dataframe(freq_table)
-
-
-if __name__=='__main__': 
-    main()
+      st.subheader("Most Frequent Values")
+      table2 = t.get_frequent()
+      freq_table = table2.astype(str)
+      st.dataframe(freq_table)
+    else:
+      st.subheader("Can not run this section as there aren't any text columns")
+      pass
