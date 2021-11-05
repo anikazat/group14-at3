b0656826b00a779e0b2637b698a4143d8b366153
junjiehua <13325889@student.uts.edu.au>
2021-11-05 17:49:56 +1100

    improve

--- src/numeric.py
+++ src/numeric.py
@@ -24,7 +24,7 @@ def read_numeric(n_data):
           if ls.get_missing() == len(ls.serie):
             st.markdown("No frequent values")
           else:
-            st.dataframe(ls.frequent().head(20))
+            st.dataframe(ls.frequent())
           n=n+1
 
         else:pass
@@ -35,46 +35,57 @@ class NumericColumn:
   col_name: str
   serie: pd.Series
   def get_name(self):
+    # Return name of selected column
     return self.col_name
  
   def get_unique(self):
+    # Return number of unique values for selected column
     unique=self.serie.value_counts().sum()
     return unique
   
   def get_missing(self):
+    # Return number of missing values for selected column
     miss=self.serie.isnull().sum()
     return miss
   
   def get_zeros(self):
+    # Return number of occurrence of 0 value for selected column
     zero= (self.serie.values == 0).sum()
     return zero
   
   def get_negatives(self):
+    # Return number of negative values for selected column
     neg= (self.serie.values <0).sum()
     return neg
 
   def get_mean(self):
+    # Return the average value for selected column
     mean= self.serie.mean()
     return mean
   
   def get_std(self):
+    # Return the standard deviation value for selected column
     std= self.serie.std()
     return std
   
   def get_min(self):
+    # Return the minimum value for selected column
     min= self.serie.min()
     return min
   
   def get_max(self):
+    # Return the maximum value for selected column
     max= self.serie.max()
     return max
   
   def get_median(self):
+    # Return the median value for selected column
     mid= self.serie.median()
     return mid
 
    
   def get_histogram(self):
+    # Return the generated histogram for selected column
     fig, ax = plt.subplots()
     ax.hist(self.serie, bins=50,edgecolor='white')
     plt.xlabel(f'{self.col_name}')
@@ -82,13 +93,15 @@ class NumericColumn:
     return fig
   
   def frequent(self):
+    # Return the Pandas dataframe containing the occurrences 
+    # and percentage of the top 20 most frequent values
     do=self.serie.value_counts(ascending=False)
     dt=self.serie.value_counts(normalize=True)
     table={'value':do.index,
             'occur':do.values,
             'freq':dt.values}
     dc=pd.DataFrame(table)
-    return dc
+    return dc.head(20)
 
   def table(self):
     test_data={'value':{'number of unique values':self.get_unique(), 
